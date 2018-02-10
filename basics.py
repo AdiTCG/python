1647a9f3afa05787557b65131cb994745c646044
AdiTCG <aditheorigin@gmail.com>
2018-02-11 02:13:05 +0530

    python basics. vairables, string, list, tuple, dictionary and print

---
+++ basics.py
@@ -0,0 +1,39 @@
+# Variable category: Below are the basic variable types we will be using often.abs
+
+# 1)Numbers: i =5; f=5.5; x=y=z=10;
+# 2)List: my_list = [1,2,3,4]
+# 3)Tuples: my_tuple = (1,2,3). Immutable.
+# 4)Dictionary: my_dict = {1:’one’, 2:’two’, 3:”three”, 4:’’’hi \n adi’’’}
+# 5)String: str = ‘adi’; str2= “””The”””
+
+#numbers 
+i=j=k=10    #multiple assignment
+print(i,j,k)    #printing variable
+
+#string
+str1='Adi'
+print(str1)
+
+str2="Hi Adi"
+print(str2)
+
+str3="""Hi Adi
+How are you"""  #printing multiline string
+print(str3)
+
+#List
+my_list = [1,2,3,4]
+print(my_list)
+
+#Tuples
+my_tuple = (1,2,3)
+print(my_tuple)
+
+my_tuple = (10,20,30)   #immutable
+print(my_tuple)
+
+#Dictionary
+my_dict = {1:'one',2:'two'}
+print(my_dict)
+
+print(my_dict.keys())
