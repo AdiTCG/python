d5c0301e7c784ea967aa97603e2f676a7488de8f
Adi Soumitra Mondal <theair@Adis-MacBook-Air.local>
2018-02-19 00:44:32 +0530

    Added numpyExample.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python
 Start with python in the simplest way, as fast as possible.
+
+#Added numpy example to do some eperiments with numpy.
---
+++ numpyExamples.py
@@ -0,0 +1,14 @@
+#Let's do some experiments to understand why to use numpy and what we can do with it.
+
+#import numpy with alias np
+import numpy as np
+
+# one dimensional array
+a=np.array([1,2,3])
+print(a)
+
+# two dimensional array
+a= np.array([(1,2,3),(4,5,6)])
+print(a)
+
+#why numpy better than list..
