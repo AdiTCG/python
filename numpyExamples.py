e81be93c2ff5a4f9fdf107c05d95f3d4d9975510
AdiTCG <aditheorigin@gmail.com>
2018-02-26 00:47:39 +0530

    why num py 1) less memory

--- numpyExamples.py
+++ numpyExamples.py
@@ -3,6 +3,8 @@
 #import numpy with alias np
 import numpy as np
 
+import sys
+
 # one dimensional array
 a=np.array([1,2,3])
 print(a)
@@ -12,3 +14,15 @@ a= np.array([(1,2,3),(4,5,6)])
 print(a)
 
 #why numpy better than list..
+#1)Less memory
+#2)faste2
+#3)Convenien3
+
+r = range(1000)
+
+print(sys.getsizeof(0)*len(r))
+
+
+d = np.arange(1000)
+print(d.size *d.itemsize)
+#so python int value is taking 28*1000 as total size, where as numpy is taking 4*1000.
